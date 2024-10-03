5d89f8345b73ed4d091b095907409b2baf5944d8
ericvenom99 <prevenakhtar@mail.com>
2024-10-03 23:32:41 +0200

    task8

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
+  }
+}
+export default divideFunction;
