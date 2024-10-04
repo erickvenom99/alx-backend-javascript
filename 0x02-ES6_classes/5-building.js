0e03bc40cc1405e20a12d9e0ad2ba8e5dbec261f
ericvenom99 <prevenakhtar@mail.com>
2024-10-04 18:22:09 +0200

    updated files

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (new.target === Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+export default Building;
