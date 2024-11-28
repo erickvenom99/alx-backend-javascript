986ab3e37770d7aa5f0e9518dcd1f5ce02b96c9a
ericvenom99 <prevenakhtar@mail.com>
2024-11-28 17:14:40 +0100

    update method files

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,10 @@
+//Asynchronus function
+const getPaymentTokenFromAPI = (success) => {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ data: 'Successful response from the API' });
+    }
+  });
+};
+
+module.exports = getPaymentTokenFromAPI;
