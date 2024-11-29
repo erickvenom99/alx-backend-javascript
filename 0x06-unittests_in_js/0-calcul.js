85d3c1c9e02d318e1d4fdc7f3e26b00611d112b3
ericvenom99 <prevenakhtar@mail.com>
2024-11-29 06:58:30 +0100

    corrected method

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -4,7 +4,7 @@
  * arg a: the first argument
  * arg b: the second argument
  */
-const calculateNumber = (a, b) => {
+const calculateNumber = (a, b = 0) => {
   const intA = a >= 0 ? Math.round(a) : Math.floor(a);
   const intB = b >= 0 ? Math.round(b) : Math.floor(b);
   return intA + intB;
