ee94c37466d2e230f5d292e1472be128ec6c01cf
ericvenom99 <prevenakhtar@mail.com>
2024-11-29 07:08:53 +0100

    corrected my test file

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,17 +8,17 @@ describe('calculateNumber', () => {
   it('should return 5 when adding 1 and 4.2', () => {
     assert.strictEqual(calculateNumber(1, 4.2), 5);
   });
-  it('should return 6 when adding 4.4 and 2.2', () => {
-    assert.strictEqual(calculateNumber(4.4, 2.2), 6);
+  it('should return 7 when adding 4.4 and 2.2', () => {
+    assert.strictEqual(calculateNumber(4.4, 2.2), 7);
   });
   it('should return 6 when adding 4.3 and 2.1', () => {
     assert.strictEqual(calculateNumber(4.3, 2.1), 6);
   });
-  it('should return 2 when adding -5.7 and 4.1', () => {
+  it('should return -2 when adding -5.7 and 4.1', () => {
     assert.strictEqual(calculateNumber(-5.7, 4.1), -2);
   });
-  it('should return -3 when adding 5.7 and -8.1', () => {
-    assert.strictEqual(calculateNumber(5.7, -8.1), -3);
+  it('should return -2 when adding 5.7 and -8.1', () => {
+    assert.strictEqual(calculateNumber(5.7, -8.1), -2);
   });
   it('should return Infinity when adding positive infinity and 2.8', () => {
     assert.strictEqual(calculateNumber(Number.POSITIVE_INFINITY, 2.8), Infinity);
