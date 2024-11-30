ae0aa5f54e50be0e43811576d1ca52cf6dcfb615
ericvenom99 <prevenakhtar@mail.com>
2024-11-30 18:16:42 +0100

    update task

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,8 +1,8 @@
-module.exports = function calculateNumbers(a, b = 0) {
-    const intA = Number(a);
-    const intB = Number(b);
-    if (Number.isNaN(intA) || Number.isNaN(intB)) {
-        throw new TypeError('Both arguments must be numbers');
-    }
-    return Math.round(intA) + Math.round(intB);
-}
+module.exports = function calculateNumbers (a, b = 0) {
+  const intA = Number(a);
+  const intB = Number(b);
+  if (Number.isNaN(intA) || Number.isNaN(intB)) {
+    throw new TypeError('Both arguments must be numbers');
+  }
+  return Math.round(intA) + Math.round(intB);
+};
